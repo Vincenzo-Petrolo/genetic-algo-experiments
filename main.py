c98416e664cee556d2d7a146cc12ba7644b7a6e7
VINCENZO PETROLO s295043 <s295043@studenti.polito.it>
2022-04-12 10:01:18 +0200

    fixed main

--- main.py
+++ main.py
@@ -7,7 +7,11 @@ def fitness_function(chromosome : chromosome):
 
 def main():
 
-    p = population.population(5,10,0.8,0)
+    # I create a population with chromosomes of length 5
+    # Starting population is 10
+    # The threshold for termination is 80%
+    # The chance of mutation during crossover is 50%
+    p = population.population(5,10,0.8,0.5)
 
     print(f"Created population {str(p)}")
     
@@ -21,6 +25,11 @@ def main():
         terminate = p.crossover()
         print(f"New population at generation {i} is {p}")
         i += 1
+    
+    print(f"Final population is {p}")
+    print(f"I'm performing the last selection for best solutions")
+    p.selection(3)
+    print(f"Best solutions {p}")
 
 if __name__ == "__main__":
     main()
